072dcd36c34629c158bc0ac3ff4ee0ec2135a063
andrew-lamont <62889624+andrew-lamont@users.noreply.github.com>
2020-04-06 15:36:07 -0400

    Add files via upload

--- AndrewMatlabDetail/TissueMeasurements/Example_processdata_illuminants.m
+++ AndrewMatlabDetail/TissueMeasurements/Example_processdata_illuminants.m
@@ -1,7 +1,7 @@
 % Example script for f_processdata_illuminants.m function
 
 %Produce 3 images based on illuminats for Bladder sample
-f_processdata_illuminants ('D:\DigitalPathology\ColorDetail\ImageData\AndrewProcessedData\BiomaxOrgan10_Bladder_M13')
+f_processdata_illuminants ('D:\DigitalPathology\ColorDetail\ImageData\AndrewProcessedData\BiomaxOrgan10_Bladder_M13\Transmittance')
 
 %Produce 3 images based on illuminants for Brain sample
-f_processdata_illuminants ('D:\DigitalPathology\ColorDetail\ImageData\AndrewProcessedData\BiomaxOrgan10_Brain_H10')
+f_processdata_illuminants ('D:\DigitalPathology\ColorDetail\ImageData\AndrewProcessedData\BiomaxOrgan10_Brain_H10\Transmittance')
